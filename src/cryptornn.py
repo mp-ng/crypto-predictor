0f69ab262f2570eadfb64808178340aaf2452f98
Ming-Pok Ng <61499762+mp-ng@users.noreply.github.com>
2022-04-09 23:42:55 +0100

    Uncomment code

--- src/cryptornn.py
+++ src/cryptornn.py
@@ -10,10 +10,8 @@ from keras.layers import Dense, Dropout, LSTM, BatchNormalization
 from keras.callbacks import TensorBoard, ModelCheckpoint
 
 import os
-import absl.logging
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-# absl.logging.set_verbosity(absl.logging.ERROR)
 
 # SECTION: PARAMETERS
 
@@ -136,46 +134,46 @@ print(f"Train data: {len(train_x)} | validation: {len(validation_x)}")
 print(f"Don't buys: {np.count_nonzero(train_y == 0)} | buys: {np.count_nonzero(train_y == 1)}")
 print(f"VALIDATION Don't buys: {np.count_nonzero(validation_y == 0)} | buys: {np.count_nonzero(validation_y == 1)}")
 
-# # SECTION: BUILD MODEL
-#
-# model = Sequential()
-# model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences=True))
-# model.add(Dropout(0.2))
-# model.add(BatchNormalization())
-#
-# model.add(LSTM(128, return_sequences=True))
-# model.add(Dropout(0.2))
-# model.add(BatchNormalization())
-#
-# model.add(LSTM(128))
-# model.add(Dropout(0.2))
-# model.add(BatchNormalization())
-#
-# model.add(Dense(32, activation="relu"))
-# model.add(Dropout(0.2))
-#
-# model.add(Dense(2, activation="softmax"))
-#
-# opt = tf.keras.optimizers.Adam(learning_rate=1e-3, decay=1e-6)
-#
-# # SECTION: COMPILE MODEL
-#
-# model.compile(loss="sparse_categorical_crossentropy",
-#               optimizer=opt,
-#               metrics=["accuracy"])
-#
-# tensorboard = TensorBoard(log_dir=f"logs/{NAME}")
-#
-# filepath = "RNN_Final-{epoch:02d}-{val_accuracy:.3f}"
-# checkpoint = ModelCheckpoint("models/{}_{}.model".format(NAME, filepath),
-#                              monitor="val_accuracy", verbose=1, save_best_only=True, mode="max")
-#
-# # SECTION: TRAIN MODEL
-#
-# history = model.fit(
-#     train_x, train_y,
-#     batch_size=BATCH_SIZE,
-#     epochs=EPOCHS,
-#     validation_data=(validation_x, validation_y),
-#     callbacks=[tensorboard, checkpoint]
-# )
+# SECTION: BUILD MODEL
+
+model = Sequential()
+model.add(LSTM(128, input_shape=(train_x.shape[1:]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(LSTM(128, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(LSTM(128))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(32, activation="relu"))
+model.add(Dropout(0.2))
+
+model.add(Dense(2, activation="softmax"))
+
+opt = tf.keras.optimizers.Adam(learning_rate=1e-3, decay=1e-6)
+
+# SECTION: COMPILE MODEL
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer=opt,
+              metrics=["accuracy"])
+
+tensorboard = TensorBoard(log_dir=f"logs/{NAME}")
+
+filepath = "RNN_Final-{epoch:02d}-{val_accuracy:.3f}"
+checkpoint = ModelCheckpoint("models/{}_{}.model".format(NAME, filepath),
+                             monitor="val_accuracy", verbose=1, save_best_only=True, mode="max")
+
+# SECTION: TRAIN MODEL
+
+history = model.fit(
+    train_x, train_y,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_data=(validation_x, validation_y),
+    callbacks=[tensorboard, checkpoint]
+)
